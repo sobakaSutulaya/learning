1a5d99062d728f7e0dbc80dcaa1ca6b21a40e86d
Nikita Ivanov <pashavintovoi@gmail.com>
2021-03-24 00:23:41 +0400

    init commit

---
+++ react-todo/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import React, { useState } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="App">
+            <header>
+                <h1>Todo's</h1>
+            </header>
+            <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
